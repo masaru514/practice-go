4f875cad95c4aada51ea704640c682ada9b6cf2d
masaru514 <415urasam@gmail.com>
2021-07-12 20:37:11 +0900

    update

--- chatapp/main.go
+++ chatapp/main.go
@@ -1,10 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 )
+
+type Ping struct {
+	Status int
+	Result string
+}
+
+func pingHandler(w http.ResponseWriter, r *http.Request) {
+	// 変数pingを定義
+	ping := Ping{http.StatusNotFound, "OK"}
+	// 変数resにjson 出力
+	res, err := json.Marshal(ping)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	// 書き込みjson
+	w.Write(res)
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the HomePage!")
@@ -12,8 +32,9 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleRequests() {
+	http.HandleFunc("/hello", pingHandler)
 	http.HandleFunc("/", homePage)
-	log.Fatal(http.ListenAndServe(":10000", nil))
+	http.ListenAndServe(":3000", nil)
 }
 
 func main() {
